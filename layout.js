d1e1ef75389948e99b942781ca53e10c62aa6e83
Luthfi <luthfiabdilah14@gmail.com>
2026-07-09 23:27:21 +0700

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,30 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  variable: "--font-geist-sans",
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+});
+
+const ovo = Ovo({
+  variable: "--font-geist-mono",
+  subsets: ["latin"], weight: ["400"]
+});
+
+export const metadata = {
+  title: "Luthfi's Portofolio",
+  description: "Personal Website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en" className="scroll-smooth"
+    >
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}>
+        {children}
+      </body>
+    </html>
+  );
+}
